6eb6cdce14aba82786844f55cbc81d0334f7d2cb
Ayan Manna <ayanmanna858@gmail.com>
2025-02-10 12:56:01 +0530

    new

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import News from "./components/News";
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
-    <>
+    <Router>
       <NavBar />
-      <News />
-    </>
+      <Routes>
+        {/* Default route for "/" */}
+        <Route path="/" element={<News category="general" />} />
+        
+        {/* Other routes */}
+        <Route exact path="/general" element={<News  key="general" category="general" />} />
+        <Route exact path="/entertainment" element={<News  key="entertainment" category="entertainment" />} />
+        <Route exact path="/health" element={<News  key="health" category="health" />} />
+        <Route exact path="/science" element={<News  key="science" category="science" />} />
+        <Route exact path="/sports" element={<News  key="sports" category="sports" />} />
+        <Route exact path="/business" element={<News  key="business" category="business"/>} />
+        <Route exact path="/technology" element={<News  key="technology" category="technology" />} />
+         
+      </Routes>
+    </Router>
   );
 }
 
